1a450f48ed0b03c7fac5bf0cfd407609686bd515
sllt <hello@sllt.me>
2022-04-03 19:28:58 +0800

    refactor httpc

--- rest/httpc/internal/loginterceptor.go
+++ rest/httpc/internal/loginterceptor.go
@@ -3,22 +3,28 @@ package internal
 import (
 	"net/http"
 
+	"go.opentelemetry.io/otel/propagation"
 	"manlu.org/tao/core/logx"
 	"manlu.org/tao/core/timex"
-	"go.opentelemetry.io/otel/propagation"
 )
 
 func LogInterceptor(r *http.Request) (*http.Request, ResponseHandler) {
 	start := timex.Now()
-	return r, func(resp *http.Response) {
+	return r, func(resp *http.Response, err error) {
 		duration := timex.Since(start)
+		if err != nil {
+			logger := logx.WithContext(r.Context()).WithDuration(duration)
+			logger.Errorf("[HTTP] %s %s - %v", r.Method, r.URL, err)
+			return
+		}
+
 		var tc propagation.TraceContext
 		ctx := tc.Extract(r.Context(), propagation.HeaderCarrier(resp.Header))
 		logger := logx.WithContext(ctx).WithDuration(duration)
 		if isOkResponse(resp.StatusCode) {
-			logger.Infof("[HTTP] %d - %s %s/%s", resp.StatusCode, r.Method, r.Host, r.RequestURI)
+			logger.Infof("[HTTP] %d - %s %s", resp.StatusCode, r.Method, r.URL)
 		} else {
-			logger.Errorf("[HTTP] %d - %s %s/%s", resp.StatusCode, r.Method, r.Host, r.RequestURI)
+			logger.Errorf("[HTTP] %d - %s %s", resp.StatusCode, r.Method, r.URL)
 		}
 	}
 }
