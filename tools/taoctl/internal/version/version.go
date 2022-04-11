37304254b1ffb67ff8ed7dc3aee74ed0fca27c28
sllt <hello@sllt.me>
2022-04-11 11:25:13 +0800

    update taoctl version

--- tools/taoctl/internal/version/version.go
+++ tools/taoctl/internal/version/version.go
@@ -6,7 +6,7 @@ import (
 )
 
 // BuildVersion is the version of taoctl.
-const BuildVersion = "1.16.7"
+const BuildVersion = "1.20.0"
 
 var tag = map[string]int{"pre-alpha": 0, "alpha": 1, "pre-bata": 2, "beta": 3, "released": 4, "": 5}
 
