b3a445407516ff841669a2efc19b6d4ecdb96a83
sllt <hello@sllt.me>
2022-12-05 00:04:35 +0800

    feature: contrib/queue added

---
+++ contrib/queue/beanstalk/config.go
@@ -0,0 +1,15 @@
+package beanstalk
+
+import "github.com/sllt/tao/core/stores/redis"
+
+type (
+	Beanstalk struct {
+		Endpoint string
+		Tube     string
+	}
+
+	DqConf struct {
+		Beanstalks []Beanstalk
+		Redis      redis.RedisConf
+	}
+)
