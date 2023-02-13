ee9ffca21ac037aa22024f4f5c55c2b8613f2aae
sllt <hello@sllt.me>
2023-02-13 10:50:14 +0800

    feature: trace http.status_code

--- rest/handler/tracinghandler.go
+++ rest/handler/tracinghandler.go
@@ -1,6 +1,11 @@
 package handler
 
 import (
+	"bufio"
+	"errors"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"net"
 	"net/http"
 	"sync"
 
@@ -12,11 +17,49 @@ import (
 	oteltrace "go.opentelemetry.io/otel/trace"
 )
 
+const (
+	traceKeyStatusCode = "http.status_code"
+)
+
 var notTracingSpans sync.Map
 
 // DontTraceSpan disable tracing for the specified span name.
 func DontTraceSpan(spanName string) {
 	notTracingSpans.Store(spanName, lang.Placeholder)
+}
+
+type traceResponseWriter struct {
+	w    http.ResponseWriter
+	code int
+}
+
+// Flush implements the http.Flusher interface.
+func (w *traceResponseWriter) Flush() {
+	if flusher, ok := w.w.(http.Flusher); ok {
+		flusher.Flush()
+	}
+}
+
+// Hijack implements the http.Hijacker interface.
+func (w *traceResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	if hijacked, ok := w.w.(http.Hijacker); ok {
+		return hijacked.Hijack()
+	}
+
+	return nil, nil, errors.New("server doesn't support hijacking")
+}
+
+func (w *traceResponseWriter) Header() http.Header {
+	return w.w.Header()
+}
+
+func (w *traceResponseWriter) Write(data []byte) (int, error) {
+	return w.w.Write(data)
+}
+
+func (w *traceResponseWriter) WriteHeader(statusCode int) {
+	w.w.WriteHeader(statusCode)
+	w.code = statusCode
 }
 
 // TracingHandler return a middleware that process the opentelemetry.
@@ -48,7 +91,21 @@ func TracingHandler(serviceName, path string) func(http.Handler) http.Handler {
 
 			// convenient for tracking error messages
 			propagator.Inject(spanCtx, propagation.HeaderCarrier(w.Header()))
-			next.ServeHTTP(w, r.WithContext(spanCtx))
+			trw := &traceResponseWriter{
+				w:    w,
+				code: http.StatusOK,
+			}
+			next.ServeHTTP(trw, r.WithContext(spanCtx))
+
+			span.SetAttributes(attribute.KeyValue{
+				Key:   traceKeyStatusCode,
+				Value: attribute.IntValue(trw.code),
+			})
+			if trw.code >= http.StatusBadRequest {
+				span.SetStatus(codes.Error, http.StatusText(trw.code))
+			} else {
+				span.SetStatus(codes.Ok, http.StatusText(trw.code))
+			}
 		})
 	}
 }
