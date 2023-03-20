cb2e5cca4e663461dd112dc9549008b1a1389078
sllt <hello@sllt.me>
2023-03-20 16:55:11 +0800

    feature: opentelemetry support AgentHost, AgentPort

--- core/trace/config.go
+++ core/trace/config.go
@@ -1,7 +1,9 @@
 package trace
 
+import "fmt"
+
 // TraceName represents the tracing name.
-const TraceName = "go-tao"
+const TraceName = "go-zero"
 
 // A Config is an opentelemetry config.
 type Config struct {
@@ -10,5 +12,16 @@ type Config struct {
 	AgentPort string  `json:",optional"`
 	Endpoint  string  `json:",optional"`
 	Sampler   float64 `json:",default=1.0"`
-	Batcher   string  `json:",default=jaeger,options=jaeger|zipkin|otlpgrpc|otlphttp"`
+	Batcher   string  `json:",default=jaeger,options=jaeger|jaegerudp|zipkin|otlpgrpc|otlphttp"`
+}
+
+func (c *Config) isAgentEndPoint() bool {
+	return len(c.AgentHost) != 0 && len(c.AgentPort) != 0
+}
+
+func (c *Config) getEndpoint() string {
+	if c.isAgentEndPoint() {
+		return fmt.Sprintf("%s:%s", c.AgentHost, c.AgentPort)
+	}
+	return c.Endpoint
 }
